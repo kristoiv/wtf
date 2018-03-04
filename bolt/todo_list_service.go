68e48aaa8eff92e8778a4d547899410c767c8b1e
Kristoffer A. Iversen <kristoffer.a.iversen@gmail.com>
2018-03-04 22:39:10 +0100

    Added List() and SetChecked(...) accessors for bolt impl.

--- bolt/todo_list_service.go
+++ bolt/todo_list_service.go
@@ -1,6 +1,8 @@
 package bolt
 
 import (
+	"time"
+
 	"code.google.com/p/go-uuid/uuid"
 	"github.com/kristoiv/wtf"
 	"github.com/kristoiv/wtf/bolt/internal"
@@ -43,14 +45,60 @@ func (s *TodoListService) Add(title string) (*wtf.Item, error) {
 	return item, nil
 }
 
-func (s *TodoListService) Items() []wtf.Item {
-	return nil
+func (s *TodoListService) Items() ([]wtf.Item, error) {
+	tx, err := s.client.db.Begin(false)
+	if err != nil {
+		return nil, err
+	}
+	defer tx.Rollback()
+
+	items := []wtf.Item{}
+	b := tx.Bucket([]byte("items"))
+	err = b.ForEach(func(_ []byte, v []byte) error {
+		item := wtf.Item{}
+		if err := internal.UnmarshalItem(v, &item); err != nil {
+			return err
+		}
+		items = append(items, item)
+		return nil
+	})
+	return items, err
 }
 
 func (s *TodoListService) SetChecked(id wtf.ItemID, checked bool) error {
-	return nil
+	tx, err := s.client.db.Begin(true)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	b := tx.Bucket([]byte("items"))
+
+	item := &wtf.Item{}
+	if data := b.Get([]byte(id)); data == nil {
+		return wtf.ErrItemNotFound
+	} else if err := internal.UnmarshalItem(data, item); err != nil {
+		return err
+	}
+
+	item.Checked = checked
+	item.Changed = time.Now().UTC()
+
+	if data, err := internal.MarshalItem(item); err != nil {
+		return err
+	} else if err := b.Put([]byte(item.ID), data); err != nil {
+		return err
+	}
+
+	return tx.Commit()
 }
 
 func (s *TodoListService) Remove(item *wtf.Item) error {
-	return nil
+	tx, err := s.client.db.Begin(true)
+	if err != nil {
+		return err
+	}
+	defer tx.Rollback()
+
+	return tx.Commit()
 }
