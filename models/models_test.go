dca18cd6272d478cea024db9e325bb9a15f84fef
Kristoffer A. Iversen <kristoffer.a.iversen@gmail.com>
2018-03-08 23:35:53 +0100

    Added additional test. Added wftd binary to gitignore

--- models/models_test.go
+++ models/models_test.go
@@ -28,3 +28,23 @@ func TestMarshalItem(t *testing.T) {
 		t.Fatalf("unexpected copy: %#v\nIS NOT EQUAL TO\n%#v", item, other)
 	}
 }
+
+func TestMarshalItemBytes(t *testing.T) {
+	now := time.Now().UTC()
+	item := wtf.Item{
+		ID:      "ID",
+		Title:   "TITLE",
+		Created: now,
+		Changed: time.Time{},
+		Checked: false,
+	}
+
+	var other wtf.Item
+	if d, err := models.MarshalItemBytes(&item); err != nil {
+		t.Fatal(err)
+	} else if err := models.UnmarshalItemBytes(d, &other); err != nil {
+		t.Fatal(err)
+	} else if !reflect.DeepEqual(item, other) {
+		t.Fatalf("unexpected copy: %#v\nIS NOT EQUAL TO\n%#v", item, other)
+	}
+}
